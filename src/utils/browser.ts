eeecc95893f024ae69b6ad8d6a92010ee3400bb5
Tom Rowbotham <tom.rowbotham@live.co.uk>
2025-01-03 12:16:40 +0000

    fix: Avoid deeplinking in TokenPocket iOS claim

--- src/utils/browser.ts
+++ src/utils/browser.ts
@@ -1,8 +1,8 @@
 export const isCompatibleBrowser = (): boolean => {
   const userAgent = navigator.userAgent.toLowerCase();
   
-  // Check for Phantom Wallet on iOS
-  if (userAgent.includes('Phantom/ios')) {
+  // These wallets support opening a new tab but will not deep link into adding an apple pass.
+  if (userAgent.includes('Phantom/ios' || userAgent.includes('TokenPocket/iOS'))) {
     return false;
   }
 
